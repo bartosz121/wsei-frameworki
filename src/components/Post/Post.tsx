dc9c7a75e9ad05d477613673bc932b5ab67a579a
bartosz121 <bmagiera121@gmail.com>
2022-10-08 01:54:40 +0200

    Add links to buttons

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,19 +1,23 @@
 import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
 
 import { IPost } from "../../types/Api";
 import ActionBtn from "../ActionBtn/ActionBtn";
 import AuthorBtn from "../AuthorBtn/AuthorBtn";
-import { UserContext } from "../../context/AppContext";
+import { AppContext } from "../../context/AppContext";
 import { trashIcon, commentIcon } from "../../icons";
 import axios from "axios";
+import Comment from "../Comment/Comment";
+import { Feed } from "../Feed/Feed";
 
 type Props = {
   data: IPost;
 };
 
-const Post = ({ data: { id, userId, title, body } }: Props) => {
+const Post = ({ data }: Props) => {
+  const { id, userId, title, body } = data;
   const [deleted, setDeleted] = useState(false);
-  const { userId: loggedInUserId, deletedPosts } = useContext(UserContext);
+  const { userId: loggedInUserId, deletedPosts } = useContext(AppContext);
 
   const deletePost = async () => {
     const res = await axios.delete(
@@ -24,30 +28,37 @@ const Post = ({ data: { id, userId, title, body } }: Props) => {
   };
 
   return (
-    <div
-      className={`my-8 rounded-xl border p-5 shadow-md w-full bg-white ${
-        deleted && "hidden"
-      }`}
-    >
-      <div className="mt-4 mb-6">
-        <div className="flex flex-row justify-between">
-          <div className="mb-3 text-xl font-bold">{title}</div>
-          {loggedInUserId === userId && (
-            <ActionBtn icon={trashIcon} onClick={deletePost} />
-          )}
+    <>
+      <div
+        className={`rounded-xl border p-5 shadow-md w-full bg-white ${
+          deleted && "hidden"
+        }`}
+      >
+        <div className="mt-4 mb-6">
+          <div className="flex flex-row justify-between">
+            <div
+              onClick={() => console.log(data)}
+              className="mb-3 text-xl font-bold"
+            >
+              {title}
+            </div>
+            {loggedInUserId === userId && (
+              <ActionBtn icon={trashIcon} onClick={deletePost} />
+            )}
+          </div>
+          <div className="text-sm text-neutral-600">{body}</div>
         </div>
-        <div className="text-sm text-neutral-600">{body}</div>
-      </div>
 
-      <div className="flex flex-row justify-around">
-        <AuthorBtn userId={userId} />
-        <ActionBtn
-          icon={commentIcon}
-          text="Comments"
-          onClick={() => console.log(id)}
-        />
+        <div className="flex flex-row justify-around">
+          <Link to={`/post/${id}`}>
+            <ActionBtn icon={commentIcon} text="Comments" />
+          </Link>
+          <Link to={`/user/${userId}`}>
+            <AuthorBtn userId={userId} />
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
