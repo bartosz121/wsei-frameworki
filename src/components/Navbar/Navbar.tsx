3b393b006a52d11f0cba5b62f2d41e16c41f7e2e
bartosz121 <bmagiera121@gmail.com>
2022-10-09 00:22:54 +0200

    Add Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+
+import { AppContext } from "../../context/AppContext";
+
+const Navbar = () => {
+  const { userId } = useContext(AppContext);
+  return (
+    <header className="bg-white shadow-lg sticky top-0 z-50">
+      <div className="mx-auto max-w-screen-xl p-4">
+        <div className="flex items-center justify-center">
+          <nav className="hidden gap-20 text-sm font-medium md:flex">
+            <Link className="text-gray-500" to="/">
+              Photos
+            </Link>
+            <Link className="text-gray-500" to="/posts">
+              Posts
+            </Link>
+            <Link className="text-gray-500" to="/users">
+              Users
+            </Link>
+            <Link className="text-gray-500" to={`/user/${userId}`}>
+              My account
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
