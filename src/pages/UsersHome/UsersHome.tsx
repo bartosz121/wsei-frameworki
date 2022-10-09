43a767675d782fe0f6ca91b92720a922c30ceffe
bartosz121 <bmagiera121@gmail.com>
2022-10-09 16:03:38 +0200

    Add UsersHome

--- src/pages/UsersHome/UsersHome.tsx
+++ src/pages/UsersHome/UsersHome.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDebounce } from "use-debounce";
 import { Feed } from "../../components/Feed/Feed";
 import UserInfo from "../../components/UserInfo/UserInfo";
 import { IUser } from "../../types/Api";
@@ -6,7 +7,29 @@ import { IUser } from "../../types/Api";
 type Props = {};
 
 const UsersHome = (props: Props) => {
-  return <Feed<IUser> component={UserInfo} apiEndpoint="users" />;
+  const [filterValue, setFilterValue] = useState("");
+  const [debouncedFilterValue] = useDebounce(filterValue, 800);
+  return (
+    <>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={filterValue}
+        onChange={(e) => setFilterValue(e.target.value)}
+        placeholder="Search by name"
+        className="my-2 w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+      />
+      <Feed<IUser>
+        component={UserInfo}
+        apiEndpoint={
+          debouncedFilterValue
+            ? `users?name_like=${debouncedFilterValue}`
+            : "users"
+        }
+      />
+    </>
+  );
 };
 
 export default UsersHome;
