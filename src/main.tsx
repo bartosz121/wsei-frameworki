1b0f212d4e4f770d8272d1a5612571e8cff78875
bartosz121 <bmagiera121@gmail.com>
2022-10-09 17:11:55 +0200

    Cleanup main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,35 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import ProtectedRoute from "./routes/ProtectedRoute/ProtectedRoute";
-import Home from "./pages/Home/Home";
-import AppRoot from "./routes/AppRoot/AppRoot";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: (
-//       <ProtectedRoute>
-//         <AppRoot />
-//       </ProtectedRoute>
-//     ),
-//     children: [
-//       {
-//         path: "/post",
-//       },
-//     ],
-//   },
-//   {
-//     path: "/login",
-//     element: <LoginForm />,
-//   },
-//   // {
-//   //   path: "/user/:id",
-//   //   element: <UserPage />,
-//   // },
-// ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
