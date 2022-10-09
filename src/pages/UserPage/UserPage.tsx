a0c307cec246880215b60206ad8b4bd6ab9e7546
bartosz121 <bmagiera121@gmail.com>
2022-10-09 14:31:43 +0200

    Add filter query param

--- src/pages/UserPage/UserPage.tsx
+++ src/pages/UserPage/UserPage.tsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import axios from "axios";
 
 import Spinner from "../../components/Spinner/Spinner";
-import { IPhoto, IPost, IUser } from "../../types/Api";
+import { IComment, IPhoto, IPost, IUser } from "../../types/Api";
 import { Feed } from "../../components/Feed/Feed";
 import Post from "../../components/Post/Post";
 import Photo from "../../components/Photo/Photo";
 import { AppContext } from "../../context/AppContext";
 import UserInfo from "../../components/UserInfo/UserInfo";
+import Comment from "../../components/Comment/Comment";
 
 const UserPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
   const [userData, setUserData] = useState<IUser | null>(null);
   const [isLoading, setIsLoading] = useState(true);
   const { userId } = useParams();
+  const [filter, setFilter] = useState(() => {
+    const queryParam = searchParams.get("filter");
+    if (queryParam) {
+      return queryParam.split(",");
+    }
+    return ["photos", "posts", "comments"];
+  });
   const {
     addedPosts,
     addedPhotos,
@@ -33,6 +42,7 @@ const UserPage = () => {
   };
 
   useEffect(() => {
+    console.log(searchParams);
     getUserData();
   }, []);
 
@@ -43,19 +53,46 @@ const UserPage = () => {
   return (
     <>
       <UserInfo data={userData!} />
-      <Feed<IPost>
-        component={Post}
-        apiEndpoint={`posts?userId=${userId}`}
-        addedArray={addedPosts}
-        deletedArray={deletedPosts}
-      />
-      <Feed<IPhoto>
-        className="mx-auto flex flex-row gap-6 flex-wrap justify-center"
-        component={Photo}
-        apiEndpoint={`photos?userId=${userId}`}
-        addedArray={addedPhotos}
-        deletedArray={deletedPhotos}
-      />
+      {filter.includes("photos") && (
+        <>
+          <h3 id="photos" className="text-2xl font-semibold border-b">
+            Photos
+          </h3>
+          <Feed<IPhoto>
+            className="mx-auto flex flex-row gap-6 flex-wrap justify-center"
+            component={Photo}
+            apiEndpoint={`photos?userId=${userId}`}
+            addedArray={addedPhotos}
+            deletedArray={deletedPhotos}
+          />
+        </>
+      )}
+      {filter.includes("posts") && (
+        <>
+          <h3 id="posts" className="text-2xl font-semibold border-b">
+            Posts
+          </h3>
+          <Feed<IPost>
+            component={Post}
+            apiEndpoint={`posts?userId=${userId}`}
+            addedArray={addedPosts}
+            deletedArray={deletedPosts}
+          />
+        </>
+      )}
+      {filter.includes("comments") && (
+        <>
+          <h3 id="comments" className="text-2xl font-semibold border-b">
+            Comments
+          </h3>
+          <Feed<IComment>
+            component={Comment}
+            apiEndpoint={`comments?email=${userData!.email}`}
+            addedArray={addedComments} // TODO filter this through postid
+            deletedArray={deletedComments}
+          />
+        </>
+      )}
     </>
   );
 };
