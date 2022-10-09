724fcf488b7b6a926778a9010c220ba219f680db
bartosz121 <bmagiera121@gmail.com>
2022-10-09 17:11:44 +0200

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,21 @@
-import React, { useState, useContext } from "react";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import Login from "./pages/Login/Login";
 import { AppContextProvider } from "./context/AppContext";
-
-import { AppContext } from "./context/AppContext";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppRoot from "./routes/AppRoot/AppRoot";
-import Home from "./pages/Home/Home";
-import PhotoHome from "./pages/PhotoHome/PhotoHome";
 import ProtectedRoute from "./routes/ProtectedRoute/ProtectedRoute";
+import Login from "./pages/Login/Login";
+import PostHome from "./pages/PostHome/PostHome";
+import PhotoHome from "./pages/PhotoHome/PhotoHome";
 import UserPage from "./pages/UserPage/UserPage";
 import PostPage from "./pages/PostPage/PostPage";
 import AlbumPage from "./pages/AlbumPage/AlbumPage";
 import UsersHome from "./pages/UsersHome/UsersHome";
 
 function App() {
-  const {
-    addedPosts,
-    addedComments,
-    addedPhotos,
-    deletedPosts,
-    deletedComments,
-    deletedPhotos,
-  } = useContext(AppContext);
-
   const queryClient = new QueryClient();
 
-  // <div className="App">
-  //   <PhotoForm />
-  //   <TextForm type="post" />
-  //   <TextForm type="comment" />
-  //   {/* <Feed feedType={FeedType.Album} /> */}
-  //   {/* <Feed feedType={FeedType.Photo} /> */}
-  //   <Feed<IPost>
-  //     component={Post}
-  //     apiEndpoint="posts"
-  //     addedArray={addedPosts}
-  //     deletedArray={deletedPosts}
-  //   />
-  // </div>
   return (
     <AppContextProvider>
       <QueryClientProvider client={queryClient}>
@@ -54,7 +30,7 @@ function App() {
               }
             >
               <Route index element={<PhotoHome />} />
-              <Route path="posts" element={<Home />} />
+              <Route path="posts" element={<PostHome />} />
               <Route path="users" element={<UsersHome />} />
               <Route path="user/:userId" element={<UserPage />} />
               <Route path="post/:postId" element={<PostPage />} />
@@ -69,34 +45,3 @@ function App() {
 }
 
 export default App;
-
-// const getFeedComponent = (fType: FeedType): FeedTypeInfo => {
-//   switch (fType) {
-//     case FeedType.Post:
-//       return {
-//         component: Post,
-//         apiEndpoint: "posts",
-//         addedArray: addedPosts,
-//         deletedArray: deletedPosts,
-//       };
-//     case FeedType.Photo:
-//       return {
-//         component: Photo,
-//         apiEndpoint: "photos?_expand=album",
-//         addedArray: addedPhotos,
-//         deletedArray: deletedPhotos,
-//       };
-//     case FeedType.Comment:
-//       return {
-//         component: Comment,
-//         apiEndpoint: "comments?_expand=post",
-//         addedArray: addedComments,
-//         deletedArray: deletedComments,
-//       };
-//     case FeedType.Album:
-//       return {
-//         component: Album,
-//         apiEndpoint: "albums",
-//       };
-//   }
-// };
