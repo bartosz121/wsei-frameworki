c7f8cb3ce7a9863699bdf727a3ca0cf8c52081e6
bartosz121 <bmagiera121@gmail.com>
2023-03-21 20:48:53 +0100

    #1 Remove context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { AppContextProvider } from "./context/AppContext";
 import AppRoot from "./routes/AppRoot/AppRoot";
 import ProtectedRoute from "./routes/ProtectedRoute/ProtectedRoute";
 import Login from "./pages/Login/Login";
@@ -13,36 +11,36 @@ import PostPage from "./pages/PostPage/PostPage";
 import AlbumPage from "./pages/AlbumPage/AlbumPage";
 import UsersHome from "./pages/UsersHome/UsersHome";
 import UserEdit from "./components/UserEdit/UserEdit";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const queryClient = new QueryClient();
 
   return (
-    <AppContextProvider>
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <AppRoot />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<PhotoHome />} />
-              <Route path="posts" element={<PostHome />} />
-              <Route path="users" element={<UsersHome />} />
-              <Route path="user/:userId" element={<UserPage />} />
-              <Route path="user/edit" element={<UserEdit />} />
-              <Route path="post/:postId" element={<PostPage />} />
-              <Route path="album/:albumId" element={<AlbumPage />} />
-            </Route>
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </AppContextProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <AppRoot />
+              </ProtectedRoute>
+            }
+          >
+            <Route index element={<PhotoHome />} />
+            <Route path="posts" element={<PostHome />} />
+            <Route path="users" element={<UsersHome />} />
+            <Route path="user/:userId" element={<UserPage />} />
+            <Route path="user/edit" element={<UserEdit />} />
+            <Route path="post/:postId" element={<PostPage />} />
+            <Route path="album/:albumId" element={<AlbumPage />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
